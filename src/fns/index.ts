704d7b43ec806b4f459df8c08d8051a56d33c171
DBL <me@banli.co>
2024-05-10 16:33:34 +0800

    feat: add shared fns `matchPermissions`

--- src/fns/index.ts
+++ src/fns/index.ts
@@ -4,3 +4,11 @@ export const getParams = (path: string = ``) => {
   const [, search = ``] = path.split(`?`)
   return new URLSearchParams(search)
 }
+
+export const matchPermissions = (
+  permissions: string[],
+  roles: { permissions: string[] }[] = [],
+): boolean => {
+  const flatten = roles.map(({ permissions }) => permissions).flat()
+  return permissions.some((permission) => flatten.includes(permission))
+}
